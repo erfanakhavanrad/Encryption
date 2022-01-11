25efa62b6d146e4e7d4fb68ad6625a91df0ceb61
erfanakhavanrad <erfanakhavanrad@outlook.com>
2022-01-11 15:04:58 +0330

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+//    This is a substitution cipher and not a OTP.
+    // OTP = One-time pad
+
+    public static void main(String[] args) {
+
+OTPEncryption otpEncryption = new OTPEncryption();
+
+    }
+}
